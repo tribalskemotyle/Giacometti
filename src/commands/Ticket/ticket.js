6a5cdfd76493a8beb01ad8801d35cd02b32fd159
tribalskemotyle <daminijan740@gmail.com>
2026-06-18 17:09:09 +0200

    Update ticket.js

--- src/commands/Ticket/ticket.js
+++ src/commands/Ticket/ticket.js
@@ -153,7 +153,7 @@ const panelMessage = interaction.options.getString("panel_message") || "Click th
 const dmOnClose = interaction.options.getBoolean("dm_on_close") !== false;
 
             const setupEmbed = createEmbed({ 
-                title: "Support Tickets", 
+                title: "Group Rank Up", 
 description: panelMessage,
                 color: getColor('info')
             });
